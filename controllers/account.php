f3f7bf69b9dfa40010e59765dd80ac46f6139dcd
MaximeMORA <pro@maximemora.com>
2019-05-07 17:38:14 +0200

    Correction des chemins de l'include
    Correction des chemins de l'include pour la redirection aprés les modifications de coordonées

--- controllers/account.php
+++ controllers/account.php
@@ -18,7 +18,7 @@ if($_SESSION['admin']==1){ //Si l'administrateur est connecté
         $pass->updatePassword($newpass);
     } 
     if ($action ==3 ){
-        include_once('email-success-change.php');
+        include_once('./views/email-success-change.php');
     }
     if ($action ==4 ){
         require_once './views/modify-email.php';
@@ -34,7 +34,7 @@ if($_SESSION['admin']==1){ //Si l'administrateur est connecté
         $pass->updateEmail($emailupdate);
     }
     if ($action ==6 ){
-        include_once('pass-success-change.php');
+        include_once('./views/pass-success-change.php');
     }
 }   
 if($_SESSION['admin']==0){ //Si l'administrateur n'est pas connecté
