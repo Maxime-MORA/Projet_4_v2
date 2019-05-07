ef007400d81dbec84c79dd0adaf3efe595d52523
MaximeMORA <pro@maximemora.com>
2019-05-07 17:39:15 +0200

    Modification du placeholder lors de la modification d'emails
    Modification du placeholder lors de la modification d'emails.

--- views/modify-email.php
+++ views/modify-email.php
@@ -2,7 +2,7 @@
     <h1 style="text-transform:uppercase;text-align:center;">Modifier votre adresse e-mail</h1>
     <form method="post" action="<?=$_SESSION["link"]?>mon-compte-5"style="padding:50px;">
         <label>Votre nouvelle adresse e-mail</label>
-         <input id ="modify-email" type="email" name="modify-email" class="form-control input_user" value="" placeholder="Votre nouveau mot de passe" style="margin-bottom:25px;">
+         <input id ="modify-email" type="email" name="modify-email" class="form-control input_user" value="" placeholder="Votre nouvelle adresse e-mail" style="margin-bottom:25px;">
         <button type="submit" style="margin-top:25px;" class="btn btn-primary btn-lg btn-block">Valider la modification</button>
     </form>
 </div>
