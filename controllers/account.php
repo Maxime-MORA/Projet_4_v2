9bebb4dc83e5f01094936f67ea999233a3cd3a2c
MaximeMORA <pro@maximemora.com>
2019-05-07 17:36:11 +0200

    Modification des redirections lors des modifications du compte administrateur
    Redirection vers une page qui affiche  :
    - xx modifié(e) avec succés
    - retour à la page d'accueil

--- controllers/account.php
+++ controllers/account.php
@@ -18,7 +18,7 @@ if($_SESSION['admin']==1){ //Si l'administrateur est connecté
         $pass->updatePassword($newpass);
     } 
     if ($action ==3 ){
-        echo('<h1>Mot de passe modifié avec succés </h1>');
+        include_once('email-success-change.php');
     }
     if ($action ==4 ){
         require_once './views/modify-email.php';
@@ -34,7 +34,7 @@ if($_SESSION['admin']==1){ //Si l'administrateur est connecté
         $pass->updateEmail($emailupdate);
     }
     if ($action ==6 ){
-        echo('<h1>Adresse e-mail modifiée avec succés </h1>');
+        include_once('pass-success-change.php');
     }
 }   
 if($_SESSION['admin']==0){ //Si l'administrateur n'est pas connecté
---
+++ views/pass-success-change.php
@@ -0,0 +1,16 @@
+<div class="jumbotron" style="height:85vh;">
+    <br />
+    <br />
+    <div style="padding:25px; background-color:rgba(255, 255, 255, 0.67);">
+        <br /> <br /> <br />
+        <h1 class="display-4">Mot de passe modifié avec succés</h1>
+        <br />
+        <p class="lead">Veuillez utiliser ce mot de passe lors de votre prochaine connexion</p>
+        <br /> <br />
+    </div>
+    <br />
+    <p class="lead">
+        <a class="btn btn-primary btn-lg btn-block" href="#" role="button">Retour à la page d'accueil</a>
+    </p>
+
+</div>
