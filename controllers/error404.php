dbc1a420d40bd30f89d9639daea2e29457bd5c90
MaximeMORA <pro@maximemora.com>
2019-05-08 17:55:47 +0200

    Ajout de commentaires

--- controllers/error404.php
+++ controllers/error404.php
@@ -1,6 +1,6 @@
 <?php
-$title="404 - Page introuvable";
-include_once('./templates/startRequire.php');
-include_once('./templates/navBar.php');
-include_once('./views/404.php');
-include_once('./templates/endRequire.php');
+$title="404 - Page introuvable"; //Titre de la page
+include_once('./templates/startRequire.php'); ///prerequis HTML
+include_once('./templates/navBar.php'); //Menu horizontal
+include_once('./views/404.php'); //Page de 404
+include_once('./templates/endRequire.php'); //Fermeture des balises HTML
