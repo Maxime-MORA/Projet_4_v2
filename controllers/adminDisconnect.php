2fd13d164325e93094ff4abba5b0418af559d716
MaximeMORA <pro@maximemora.com>
2019-05-08 17:55:10 +0200

    Ajout de commentaires

--- controllers/adminDisconnect.php
+++ controllers/adminDisconnect.php
@@ -1,5 +1,5 @@
 <?php
-$title="Déconnexion";
-include_once('./templates/startRequire.php');
-include_once('./models/deconnexion.php');
+$title="Déconnexion"; //Titre de la page
+include_once('./templates/startRequire.php'); //Requis HTML ( head + ouverture des balises )
+include_once('./models/deconnexion.php'); //Déconnexion - Session['admin'] = 0
 ?>
