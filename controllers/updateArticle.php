c8ae64ca34de79dc22831bdb1a65fe7e116f41b9
MaximeMORA <pro@maximemora.com>
2019-05-08 17:56:42 +0200

    Ajout de commentaires

--- controllers/updateArticle.php
+++ controllers/updateArticle.php
@@ -1,19 +1,18 @@
 <?php
-$title="Mise à jour de l'article - ".$idArticle;
-include_once('./templates/startRequire.php');
-include_once('./templates/navBar.php');
-//Connexion à la base de donnéees
-include_once('./models/databaseConnect.php');
+$title="Mise à jour de l'article - ".$idArticle; //Titre de la page
+include_once('./templates/startRequire.php'); //Prerequis HTML
+include_once('./templates/navBar.php'); //Menu horizontal
+include_once('./models/databaseConnect.php'); //Connexion à la base de donnéees
 $database = new dataBase;
 $database->connectTo();
 require_once('./models/postManager.php');
 $post = new Post;
-$post->updateOne($idArticle);
+$post->updateOne($idArticle); //Mise à jour de l'article dont l'id = $idArticle
 if($_SESSION['admin']==1){ //Si l'administrateur est connecté
-     require_once './views/update.php';   
+     require_once './views/update.php'; //Chargement de la vue de modification d'article
 }
 if($_SESSION['admin']==0){ //Si l'administrateur n'est pas connecté
-     header('Location:'.$_SESSION["link"].'connexion'); 
+     header('Location:'.$_SESSION["link"].'connexion'); //Redirection vers la page de connexion
 }
 
 
