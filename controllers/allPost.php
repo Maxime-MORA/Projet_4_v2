84706f99b1c67ab98b051da9cc8d8a16de8a5500
MaximeMORA <pro@maximemora.com>
2019-05-08 17:55:20 +0200

    Ajout de commentaires

--- controllers/allPost.php
+++ controllers/allPost.php
@@ -1,14 +1,13 @@
 <?php
-$title="Tout les articles";
-include_once('./templates/startRequire.php');
-include_once('./templates/navBar.php');
-//Connexion à la base de donnéees
-include_once('./models/databaseConnect.php');
+$title="Tout les articles"; //Titre de la page
+include_once('./templates/startRequire.php'); //Prérequis
+include_once('./templates/navBar.php'); //Menu horizontal
+include_once('./models/databaseConnect.php'); //Connexion à la base de donnéees
 $database = new dataBase;
 $database->connectTo();
 require_once('./views/allPost.php');
 require_once('./models/postManager.php');
 $post = new Post;
-$post->getAll();
-include_once('./templates/footer.php');
-include_once('./templates/endRequire.php');
+$post->getAll(); //Récupération de tout les articles
+include_once('./templates/footer.php'); // Intégration du footer
+include_once('./templates/endRequire.php'); //Fermeture des balises
