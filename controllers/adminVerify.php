898cef5d660519b8775afb7f3b748ba5b036a10d
MaximeMORA <pro@maximemora.com>
2019-05-08 17:55:16 +0200

    Ajout de commentaires

--- controllers/adminVerify.php
+++ controllers/adminVerify.php
@@ -1,13 +1,12 @@
 <?php
-$title="Vérification";
-include_once('./templates/startRequire.php');
-include_once('./templates/navBar.php');
-//Connexion à la base de donnéees
-include_once('./models/databaseConnect.php');
+$title="Vérification"; //Titre de la page
+include_once('./templates/startRequire.php'); // Prérequis
+include_once('./templates/navBar.php'); // Menu horizontal
+include_once('./models/databaseConnect.php'); //Connexion à la base de donnéees
 $database = new dataBase;
 $database->connectTo();
-require_once './models/adminVerification.php';
-$email = $_POST['email'];
-$password = $_POST['password'];
+require_once ('./models/adminVerification.php'); //Vérification des informations de l'administrateur
+$email = $_POST['email']; // Récupération de l'e-mail saisi
+$password = $_POST['password']; //Récupération du MDP saisi
 $verif = new Admin;
-$verif->verif($email, $password);
+$verif->verif($email, $password); //Lance la vérification avec les infos récupérées
