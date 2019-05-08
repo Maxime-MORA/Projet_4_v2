5316c035a01cae93e76a0674cd6ca54c63fa6803
MaximeMORA <pro@maximemora.com>
2019-05-09 00:01:15 +0200

    Ajout des commentaires

--- models/adminVerification.php
+++ models/adminVerification.php
@@ -1,22 +1,20 @@
 <?php
 class Admin extends dataBase
 {
-public function verif($email, $password){
+public function verif($email, $password){ //Vérification des données entrées lors de la connexion
 $db=$this->connectTo(); 
 //Récupération des informations utilisateurs
 $retour = $db->prepare('SELECT email, password FROM users WHERE id=1');
 $retour->execute();
 while ($data = $retour->fetch())
      { 
-     //Comparaison des informations utilisateurs
-   if($data["email"]==$email && $data["password"]== sha1($password)){
-       //Si les informations sont bonnes / L'administrateur est connecté
-       $_SESSION['admin']=1;
-       header('Location:'.$_SESSION["link"]);
+   if($data["email"]==$email && $data["password"]== sha1($password)){ //Si l'e-mail et le mot de passe ( crypté en sha1 ) correspondent aux données de la BDD
+       //L'administrateur est connecté
+       $_SESSION['admin']=1; //Changement de la valeur admin = 1 = ADMINISTRATEUR
+       header('Location:'.$_SESSION["link"]); //Redirection vers la page d'accuiel
        
    }
-    else{
-        //Si les informations sont mauvaises / L'administrateur n'est pas connecté
+    else{//Si les informations sont mauvaises / L'administrateur n'est pas connecté
         $_SESSION['admin']=0;
         // + Messages d'erreurs
         echo('<div class="container" style="text-align:center;padding-top:100px;">');
