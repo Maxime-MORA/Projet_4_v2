8527298191cebcbc7215d71768fa01b00ea8f4a8
MaximeMORA <pro@maximemora.com>
2019-05-08 17:56:18 +0200

    Ajout de commentaires

--- controllers/passwordLost.php
+++ controllers/passwordLost.php
@@ -1,6 +1,6 @@
 <?php
-$title="Mot de passe oublié";
-include_once('./templates/startRequire.php');
-include_once('./templates/navBar.php');
-include_once('./views/passwordLost.php');
-include_once('./templates/endRequire.php');
+$title="Mot de passe oublié"; //Page de mot de passe oublié
+include_once('./templates/startRequire.php'); //Prérequis HTML
+include_once('./templates/navBar.php'); //Menu horizontal
+include_once('./views/passwordLost.php'); //Chargement de la vue - Mot de passe oublié 
+include_once('./templates/endRequire.php'); //Fermeture des balises HTML
