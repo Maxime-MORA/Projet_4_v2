6d8b2002c810f4ba09286d36a83de06eb8ef989f
MaximeMORA <pro@maximemora.com>
2019-05-08 17:55:25 +0200

    Ajout de commentaires

--- controllers/bibliographiePage.php
+++ controllers/bibliographiePage.php
@@ -1,7 +1,7 @@
 <?php
-$title="Bibliographie";
-include_once('./templates/startRequire.php');
-include_once('./templates/navBar.php');
-include_once('./views/bibliographiePage.php');
-include_once('./templates/footer.php');
-include_once('./templates/endRequire.php');
+$title="Bibliographie"; //Titre de la page
+include_once('./templates/startRequire.php'); //Prérequis
+include_once('./templates/navBar.php'); //Menu horizontal
+include_once('./views/bibliographiePage.php'); //Récupération de la page 'bibliographie'
+include_once('./templates/footer.php'); //Intégration du footer
+include_once('./templates/endRequire.php'); //Fermeture des balises
