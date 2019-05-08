c3875429fb829be2bb91a4d91223ae9d5fefccb5
MaximeMORA <pro@maximemora.com>
2019-05-08 17:56:23 +0200

    Ajout de commentaires

--- controllers/passwordLostVerification.php
+++ controllers/passwordLostVerification.php
@@ -1,12 +1,11 @@
 <?php
-$title="E-mail envoyé";
-include_once('./templates/startRequire.php');
-include_once('./templates/navBar.php');
-//Connexion à la base de donnéees
-include_once('./models/databaseConnect.php');
+$title="E-mail envoyé"; //Titre de la page
+include_once('./templates/startRequire.php'); //Prérequis HTML
+include_once('./templates/navBar.php'); //Menu horizontal
+include_once('./models/databaseConnect.php'); //Connexion à la base de donnéees
 $database = new dataBase;
 $database->connectTo();
-include_once('./models/passwordGeneration.php');
+include_once('./models/passwordGeneration.php'); //Génération d'un nouveau mot de passe
 $comment = new password;
-$comment->emailVerification($_POST['recovery-email']);
-include_once('./templates/endRequire.php');
+$comment->emailVerification($_POST['recovery-email']); //Vérification de l'email ( e-mail saisi = e-mail de l'admin)
+include_once('./templates/endRequire.php'); //Fermeture des balises HTML
