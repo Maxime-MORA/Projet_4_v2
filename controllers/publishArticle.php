f523b9c224f9b8c396c43620187794ffa3e51e4a
MaximeMORA <pro@maximemora.com>
2019-05-08 17:56:27 +0200

    Ajout de commentaires

--- controllers/publishArticle.php
+++ controllers/publishArticle.php
@@ -1,15 +1,14 @@
 <?php
-$title="Publier un article";
-include_once('./templates/startRequire.php');
-include_once('./templates/navBar.php');
-//Connexion à la base de donnéees
-include_once('./models/databaseConnect.php');
+$title="Publier un article"; //Titre de la page
+include_once('./templates/startRequire.php'); //Prerequis HTML
+include_once('./templates/navBar.php'); //Menu horizontal
+include_once('./models/databaseConnect.php'); //Connexion à la base de donnéees
 $database = new dataBase;
 $database->connectTo();
 if($_SESSION['admin']==1){ //Si l'administrateur est connecté
-     require_once './views/publish.php';   
+     require_once './views/publish.php';   //Chargement du formulaire de mise en ligne d'un article - TinyMCE
 }
 if($_SESSION['admin']==0){ //Si l'administrateur n'est pas connecté
-     header('Location:'.$_SESSION["link"].'connexion'); 
+     header('Location:'.$_SESSION["link"].'connexion'); //Redirection vers la page de connexion
 }
-include_once('./templates/endRequire.php');
+include_once('./templates/endRequire.php'); //Fermeture des balises HTML
