4b1da3113bc7a0fe9a976f709ac81eba51d126c4
MaximeMORA <pro@maximemora.com>
2019-05-08 17:55:43 +0200

    Ajout de commentaires

--- controllers/deleteSignal.php
+++ controllers/deleteSignal.php
@@ -1,17 +1,16 @@
 <?php
-$title="suppréssion du signalement";
-include_once('./templates/startRequire.php');
-//Connexion à la base de donnéees
-include_once('./models/databaseConnect.php');
+$title="suppréssion du signalement"; //Titre de la page
+include_once('./templates/startRequire.php'); //Prérequis HTML
+include_once('./models/databaseConnect.php'); //Connexion à la base de donnéees
 $database = new dataBase;
 $database->connectTo();
 if($_SESSION['admin']==1){ //Si l'administrateur est connecté
      include_once('./models/commentsManager.php');
      $comment = new Comment;
-     $comment->signalDelete($commentid);
-     header('Location:'.$_SESSION["link"].'article');
+     $comment->signalDelete($commentid); //Sinalement de l'article dont l'id = $commentid
+     header('Location:'.$_SESSION["link"].'article'); //Redirection vers la page d'articles
 }
 if($_SESSION['admin']==0){ //Si l'administrateur n'est pas connecté
-     header('Location:'.$_SESSION["link"].'connexion'); 
+     header('Location:'.$_SESSION["link"].'connexion'); //Redirection vers la page de connexion
 }
-include_once('./templates/endRequire.php');
+include_once('./templates/endRequire.php'); //Fermeture des balises HTML
