cd9afc9f333bb232c143a9e68dc61b1472ea9efc
MaximeMORA <pro@maximemora.com>
2019-05-08 13:31:47 +0200

    Amélioration du responsive pour les iphones SE
    Réduction des textes,
    Réductions des paddings...

--- views/publish.php
+++ views/publish.php
@@ -1,6 +1,6 @@
 <div class="container" style="padding-top:50px;">
     <h1 style="text-transform:uppercase;text-align:center;">Publier un article</h1>
-    <form method="post" action="<?=$_SESSION["link"]?>send-article"style="padding:50px;;">
+    <form method="post" action="<?=$_SESSION["link"]?>send-article"style="padding:15px;">
         <label>Titre de l'article</label>
         <input type="text" name="article-title" class="form-control input_user" value="" placeholder="Titre de l'article" style="margin-bottom:25px;">
         <label>Contenu de l'article</label>
