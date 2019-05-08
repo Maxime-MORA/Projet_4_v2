21e74623b4e8b5681d99a016c4aa8107d4b6e33f
MaximeMORA <pro@maximemora.com>
2019-05-08 17:56:09 +0200

    Ajout de commentaires

--- controllers/modifyArticleSend.php
+++ controllers/modifyArticleSend.php
@@ -1,17 +1,16 @@
 <?php
-$title="Envoi de l'article modifié";
-include_once('./templates/startRequire.php');
-include_once('./templates/navBar.php');
-//Connexion à la base de donnéees
-include_once('./models/databaseConnect.php');
+$title="Envoi de l'article modifié"; //Titre de la page
+include_once('./templates/startRequire.php'); //Prerequis HTML
+include_once('./templates/navBar.php'); //Menu horizontal
+include_once('./models/databaseConnect.php'); //Connexion à la base de donnéees
 $database = new dataBase;
 $database->connectTo();
 if($_SESSION['admin']==1){ //Si l'administrateur est connecté
      require_once './models/postManager.php';
      $post = new Post;
-     $post->modifyArticle($_POST['modify-title'], $_POST['modify-content'], $idArticle);
+     $post->modifyArticle($_POST['modify-title'], $_POST['modify-content'], $idArticle); //Modifier les articles sur la BDD - Avec les informations récupérés
 }
 if($_SESSION['admin']==0){ //Si l'administrateur n'est pas connecté
-     header('Location:'.$_SESSION["link"].'connexion'); 
+     header('Location:'.$_SESSION["link"].'connexion'); //Redirection vers la page de connexion
 }
-include_once('./templates/endRequire.php');
+include_once('./templates/endRequire.php'); //Fermeture des balises HTML
