43dc7cc7f8ef00bd98345a071bfe9ef8fe22be48
MaximeMORA <pro@maximemora.com>
2019-05-08 17:56:38 +0200

    Ajout de commentaires

--- controllers/signalComment.php
+++ controllers/signalComment.php
@@ -1,12 +1,11 @@
 <?php
-$title="signalement du commentaire";
-include_once('./templates/startRequire.php');
-//Connexion à la base de donnéees
-include_once('./models/databaseConnect.php');
+$title="Signalement du commentaire"; //Titre de la page
+include_once('./templates/startRequire.php'); //Prerequis HTML
+include_once('./models/databaseConnect.php'); //Connexion à la base de donnéees
 $database = new dataBase;
 $database->connectTo();
 include_once('./models/commentsManager.php');
 $comment = new Comment;
-$comment->signalComment($commentid);
-include_once('./templates/endRequire.php');
-header('Location:'.$_SESSION["link"].'article');
+$comment->signalComment($commentid); //Signalement du commentaire dont l'id = $commentid
+include_once('./templates/endRequire.php'); //Fermeture des balises HTML
+header('Location:'.$_SESSION["link"].'article'); //Redirection vers la page des articles
