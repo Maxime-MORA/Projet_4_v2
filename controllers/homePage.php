b5b6cc60193fa650261c504ebf0d190a49ca6543
MaximeMORA <pro@maximemora.com>
2019-05-08 17:55:52 +0200

    Ajout de commentaires

--- controllers/homePage.php
+++ controllers/homePage.php
@@ -1,15 +1,14 @@
 <?php
-$title="Page d'accueil";
-include_once('./templates/startRequire.php');
-include_once('./templates/navBar.php');
-include_once('./templates/heroBanner.php');
-//Connexion à la base de donnéees
-include_once('./models/databaseConnect.php');
+$title="Page d'accueil"; //Titre de la page
+include_once('./templates/startRequire.php'); //Prerequis HTML
+include_once('./templates/navBar.php'); //Menu horizontal
+include_once('./templates/heroBanner.php'); //Banniere
+include_once('./models/databaseConnect.php'); //Connexion à la base de donnéees
 $database = new dataBase;
 $database->connectTo();
-require_once('./views/lastPost.php');
+require_once('./views/lastPost.php'); //Vue des derniers articles
 require_once('./models/postManager.php');
 $post = new Post;
-$post->getLast(3);
-include_once('./templates/footer.php');
-include_once('./templates/endRequire.php');
+$post->getLast(3); //Chargement des derniers articles (3)
+include_once('./templates/footer.php'); //Insertion du footer
+include_once('./templates/endRequire.php'); //Fermeture des balises HTML
